914d1aab792e4d118f9221ef877652ba0df4da1e
Pratik Korane <pratik894720@gmail.com>
2024-12-27 21:55:42 +0530

    Assignment_02

---
+++ Assignments/Assignment_02/Assignment_2.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._2
+{
+    internal class Program
+    {
+
+        static void reverseArray(Student []students)
+        {
+            for (int i = students.Length-1; i >=0; i--) {
+                students[i].printInfo();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of array of students");
+            int size=Convert.ToInt32(Console.ReadLine());
+            Student[] student = new Student[size];
+
+            for (int i = 0; i < student.Length; i++) {
+                student[i].acceptInfo();
+            }
+
+            for (int i = 0; i < student.Length; i++) {
+                student[i].printInfo();
+            }
+
+            Console.WriteLine("After reversing the array");
+            Console.WriteLine("----------------------------------------------------------");
+            reverseArray(student);
+        }
+    }
+}
---
+++ Assignments/Assignment_02/Assignment_2.2/Student.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment_2._2
+{
+    
+    public struct Student
+    {
+
+        private string _Name;
+        private bool _Gender;
+        private int _Age;
+        private int _Std;
+        private char _Div;
+        private double _Marks;
+
+
+
+        public Student(string name, bool gender, int age, int std, char div, double marks)
+        {
+            _Name = name;
+            _Gender = gender;
+            _Age = age;
+            _Std = std;
+            _Div = div;
+            _Marks = marks;
+        }
+
+
+
+
+
+        public void acceptInfo()
+        {
+            Console.WriteLine("Enter name");
+            _Name = Console.ReadLine();
+
+            bool flag = true;
+            while (flag)
+            {
+
+                Console.WriteLine("Enter 1.MALE 2.FEMALE");
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        {
+                            _Gender = true;
+                            flag = false;
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            _Gender = false;
+                            flag = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Wrong choice");
+                            break;
+                        }
+                }
+            }
+
+
+            Console.WriteLine("Engter Age");
+            _Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter std");
+            _Std = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter div");
+            _Div = char.Parse(Console.ReadLine());
+            Console.WriteLine("Enter mark");
+            _Marks = int.Parse(Console.ReadLine());
+
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("Name: " + _Name);
+
+            if (_Gender == true)
+            {
+                Console.WriteLine("Gender: Male");
+            }
+            else
+            {
+                Console.WriteLine("Gender: Female");
+            }
+
+            Console.WriteLine("Age: " + _Age);
+            Console.WriteLine("Std: " + _Std);
+            Console.WriteLine("Div: " + _Div);
+            Console.WriteLine("Marks: " + _Marks);
+        }
+
+
+
+
+
+
+        public double Marks
+        {
+            get { return _Marks; }
+            set { _Marks = value; }
+        }
+
+
+        public char Div
+        {
+            get { return _Div; }
+            set { _Div = value; }
+        }
+
+
+        public int Std
+        {
+            get { return _Std; }
+            set { _Std = value; }
+        }
+
+
+        public int Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+
+
+        public bool Gender
+        {
+            get { return _Gender; }
+            set { _Gender = value; }
+        }
+
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+
+
+
+
+
+
+
+    }
+}
